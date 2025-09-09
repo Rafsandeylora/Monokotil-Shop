9c28bc2a170dbbf34f8db8bc3a83734ab334ed57
rafsandeylora <rafsanjanirafsanjani8@gmail.com>
2025-09-09 14:08:02 +0700

    membuat tampilan html

---
+++ main/models.py
@@ -0,0 +1,26 @@
+import uuid
+from django.db import models
+
+class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('jersey', 'Jersey'),
+        ('hat', 'Hat'),
+        ('merchandise', 'Merchandise'),
+        ('ball', 'Ball'),
+        ('handsign', 'Hansign'),
+        ('shoes', 'Shoes'),
+    ]
+    
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
+    stocks = models.IntegerField()
+    rarity = models.IntegerField()
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
+    thumbnail = models.URLField(blank=True,null=True)
+    is_featured = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name
+    
+  
